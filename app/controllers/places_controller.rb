70ecc1298def80e456098f31c824010b5b97ee0c
tsemrau <tyler.semrau@kellogg.northwestern.edu>
2022-02-23 05:25:32 +0000

    committ

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,19 @@
+class PlacesController < ApplicationController
+    def index
+        @places = Place.all
+    end
+
+    def create
+        @place = Place.new(params["place"])
+        @place.save
+        redirect_to "/places"
+    end
+
+    def new
+        @place = Place.new
+    end
+
+    def show
+        @place = Place.find(params["id"])
+    end
+end
